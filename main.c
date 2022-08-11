47a2b58709553c7498f53ec8cac2c9df0a4bd60e
Alba Castano <acastano@student.hive.fi>
2022-08-11 16:27:41 +0300

    working on a function that displays a files(passed as an argument) info

--- main.c
+++ main.c
@@ -29,7 +29,6 @@ int	list_dir_contents(void)
 	const char	*filename;
 	DIR	*dirp = NULL;
 	struct dirent	*dp = NULL;
-//	int	i = 0;
 
 	filename = "./";
 	dirp = opendir(filename);
@@ -54,6 +53,49 @@ int	list_dir_contents(void)
 	return (0);
 }
 
+int	list_dir_contents_no_dot(void)
+{
+	const char	*filename;
+	DIR	*dirp = NULL;
+	struct dirent	*dp = NULL;
+
+	filename = "./";
+	dirp = opendir(filename);
+	if (dirp == NULL)
+	{
+		printf("DEBUG: dirp returned NULL\n");
+		return (0);
+	}
+	else
+	{
+		printf("DEBUG: dirp returned a pointer\n");
+//		dp = readdir(dirp);
+//		if (dp == NULL)
+//			printf("DEBUG: dp returned NULL\n");
+		while ((dp = readdir(dirp)) != NULL)
+		{
+			if (dp->d_name[0] != '.')
+				printf("%s\n", dp->d_name);
+		}
+	}
+	closedir(dirp);
+	return (0);
+}
+
+int	file_info(char *filename)
+{
+	printf("The passed file is: %s\n", filename);
+	printf("Name: \n");// a_crazy_file
+	printf("Type: \n");//File
+	printf("Modes: \n");//rwxr-xr-x
+	printf("Number of links: \n");//1
+	printf("Owner: \n");//zaz
+	printf("Group: \n");//staff
+	printf("Size: \n");//2142 octets
+	printf("Last modification date: \n");//Sep 17 23:42
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 /*	const char	*filename;
@@ -63,7 +105,15 @@ int	main(int argc, char **argv)
 //		return (0);
 
 	if ((argc == 1) && argv)
+	{
 		list_dir_contents();
+		printf("\nDEBUG: now without dot files\n");
+		list_dir_contents_no_dot();
+	}
+	if (argc == 2)
+	{
+		file_info(argv[1]);
+	}
 //		check_args(argc, argv);
 /*	filename = argv[1];
 	dp = opendir(filename);
